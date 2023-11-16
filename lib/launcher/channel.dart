686f87384440d714b482dfc3fd1f63ca6b6f2453
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:06:15 +0530

    Update channel.dart

--- lib/launcher/channel.dart
+++ lib/launcher/channel.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/services.dart';
 
 const platform = MethodChannel('echo_tunes');
+
+Future<void> launchMusicPlayer() async {
+  try {
+    await platform.invokeMethod('launchMusicPlayer');
+  } on PlatformException catch (e) {
+    print("Failed to launch music player: ${e.message}");
+  }
+}
