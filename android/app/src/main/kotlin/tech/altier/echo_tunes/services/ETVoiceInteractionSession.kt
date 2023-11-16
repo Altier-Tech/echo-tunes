5d01ef52973b2296b5df8dfed25ac693e638feb1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:01:29 +0530

    Update ETVoiceInteractionSession.kt

--- android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionSession.kt
+++ android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionSession.kt
@@ -5,7 +5,7 @@ import android.service.voice.VoiceInteractionSession
 class ETVoiceInteractionSession(context: Context) : VoiceInteractionSession(context) {
     override fun onLaunchVoiceAssistFromKeyguard() {
         // TODO Handle the voice command to launch the app
-        val intent = Intent(context, YourFlutterMainActivity::class.java)
+        val intent = Intent(context, MainActivity::class.java) // TODO fix
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(intent)
         finish()
