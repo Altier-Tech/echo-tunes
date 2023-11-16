850272c8b09ef8825a27c679494dfb905adcac27
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:08:44 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/tech/altier/echo_tunes/MainActivity.kt
+++ android/app/src/main/kotlin/tech/altier/echo_tunes/MainActivity.kt
@@ -5,5 +5,24 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        // Set up method channel
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "your_channel_name")
+            .setMethodCallHandler { call, result ->
+                if (call.method == "launchMusicPlayer") {
+                    // Handle the method call, e.g., launch the music player
+                    launchMusicPlayer()
+                    result.success(null) // You can send a result back to Dart if needed
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    private fun launchMusicPlayer() {
+        // Implement the logic to launch the music player
+    }
 }
