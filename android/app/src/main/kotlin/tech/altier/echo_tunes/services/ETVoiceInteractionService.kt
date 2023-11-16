94bb07271b6bcc8f6ffeb4e20cd366a6e3c8e27c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:07:09 +0530

    Update ETVoiceInteractionService.kt

--- android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionService.kt
+++ android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionService.kt
@@ -3,6 +3,10 @@ import android.content.Intent
 import android.service.voice.VoiceInteractionService
 import android.service.voice.VoiceInteractionSession
 import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
 
 class ETVoiceInteractionService : VoiceInteractionService() {
     override fun onNewSession(args: Bundle?): VoiceInteractionSession {
