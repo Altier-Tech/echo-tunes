a405138cc8caf7abde39d8aeb9cc6d80fd92e033
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:07:12 +0530

    Update ETVoiceInteractionService.kt

--- android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionService.kt
+++ android/app/src/main/kotlin/tech/altier/echo_tunes/services/ETVoiceInteractionService.kt
@@ -3,6 +3,7 @@ import android.content.Intent
 import android.service.voice.VoiceInteractionService
 import android.service.voice.VoiceInteractionSession
 import android.os.Bundle
+
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
