fb9d3aa26e8b8980f6b2cea42eec18dd15cdc05d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-16 18:08:19 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/tech/altier/echo_tunes/MainActivity.kt
+++ android/app/src/main/kotlin/tech/altier/echo_tunes/MainActivity.kt
@@ -1,6 +1,9 @@
 package tech.altier.echo_tunes
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
 }
