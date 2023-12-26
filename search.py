648c97aeb872b5bf08600c8bb9cb4b16055c418f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-26 14:49:11 +0530

    Update search.py

--- search.py
+++ search.py
@@ -12,4 +12,11 @@ def search_songs(search_term):
     search_term_words = [word for word in search_term_words if word != "the"]
     search_term_words = [word for word in search_term_words if word != "play"]
 
-    
+    for song in songs_dict.keys():
+        song_words = song.split(" ")
+        song_words = [word for word in song_words if word != "the"]
+
+        if set(search_term_words).issubset(set(song_words)):
+            return songs_dict[song]
+
+    return None
