a8080b240c15edbb6a90108025271e9fa65dc6f9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-26 09:59:08 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import speech_recognition as sr
+
+# Initialize recognizer
+r = sr.Recognizer()
+
+
+# Function to recognize speech
+def recognize_speech():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            return text.lower()
+        except:
+            return ""
+
+
+# Function to launch script
+def launch_script():
+    os.system('python player.py')
+
+
+# Main loop
+while True:
+    command = recognize_speech()
+    if command == 'launch music player':
+        launch_script()
