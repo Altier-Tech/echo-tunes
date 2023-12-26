e678f005482f2f07d71da567c729005c5d95126b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-26 14:51:34 +0530

    Update player.py

--- player.py
+++ player.py
@@ -205,7 +205,6 @@ def handle_voice_commands():
             new_volume = current_volume - 10 if current_volume - 10 > 0 else 0
             volume_scale.set(new_volume)
         elif command.split(" ")[0] == "play" and command.split(" ")[1] == "song" and len(command.split(" ")) > 2:
-            song_name = command.split(" ", 2)[2]
             song_index = search_songs(song_name)
             if song_index is not None:
                 play_by_index(song_index)
