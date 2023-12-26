6e8a58173e1561349061ef7c27bb9cda62882dee
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-26 14:47:43 +0530

    Update search.py

--- search.py
+++ search.py
@@ -3,3 +3,12 @@ songs_dict = {}
 
 def add_song(title, index):
     songs_dict[index] = title
+
+
+def search_songs(search_term):
+    search_term = search_term.lower()
+
+    search_term_words = search_term.split(" ")
+    search_term_words = [word for word in search_term_words if word != "the"]
+    search_term_words = [word for word in search_term_words if word != "play"]
+
