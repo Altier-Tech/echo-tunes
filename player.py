0ec7a80cb076e9aa9c4ec920fc968f471344a472
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-26 14:41:43 +0530

    Update player.py

--- player.py
+++ player.py
@@ -83,11 +83,11 @@ def play_song():
 
 
 def play_by_index(index):
-        song = playlist.item(playlist.get_children()[index])['values'][1]
-        song_path = os.path.join(songs_dir, song + '.mp3')
-        pygame.mixer.music.load(song_path)
-        pygame.mixer.music.play()
-        status_label.config(text="Status: Playing")
+    song = playlist.item(playlist.get_children()[index])['values'][1]
+    song_path = os.path.join(songs_dir, song + '.mp3')
+    pygame.mixer.music.load(song_path)
+    pygame.mixer.music.play()
+    status_label.config(text="Status: Playing")
 
 
 def next_song():
