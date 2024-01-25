62079e0c1637ffdde96be9006c13545d6914089b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 17:02:32 +0530

    Update player.py

--- player.py
+++ player.py
@@ -279,6 +279,8 @@ play_icon = ImageTk.PhotoImage(img)
 play_button = tk.Button(control_frame, image=play_icon, command=play_song, bg='#7E84F7', compound=tk.CENTER)
 play_button.pack(side=tk.LEFT)
 
+
+
 # Modify volume_scale to call set_volume when the scale is moved
 volume_scale = tk.Scale(control_frame, from_=0, to=100, orient=tk.HORIZONTAL, label="Volume", command=set_volume,
                         bg='#7E84F7')
